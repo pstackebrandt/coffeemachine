06308a83026a86106c643a2bd516acee2f3db01a
Peter Stackebrandt <peter.stackebrandt@gmail.com>
2021-01-22 22:15:23 +0100

    Add coffeemachinr Part 2
    get ingredients
    cook coffee
    tell about further possible cup or not cooked cups becaus of not enout ingredients

--- Coffee Machine/task/src/machine/CoffeeMachine.kt
+++ Coffee Machine/task/src/machine/CoffeeMachine.kt
@@ -1,15 +1,89 @@
 package machine
 
+import kotlin.math.min
+
+
 class CoffeeMachine {
+    private val ingredients = Ingredients()
+
     fun loopMenu() {
         var loop: Boolean
 
         do {
-            printIngredients(howMuchCoffee())
+            sumIngredients(getAdditionalIngredients())
+            printPossibleCoffeeAnswer(howMuchCoffeeRequired())
+//            printIngredients(howMuchCoffee())
             loop = false
         } while (loop)
     }
 
+    private fun printPossibleCoffeeAnswer(requiredCoffeeCount: Int) {
+        println(getPossibleCoffeeAnswer(requiredCoffeeCount))
+    }
+
+    private fun getPossibleCoffeeAnswer(requiredCoffeeCount: Int): String {
+        val cupsCoffeeMadeCount = makeCupsOfCoffee(requiredCoffeeCount)
+        val furtherPossibleCoffeeCount = howMuchCoffeePossible(ingredients)
+
+        return when {
+            furtherPossibleCoffeeCount > 0 -> "Yes, I can make that amount of coffee " +
+                    "(and even $furtherPossibleCoffeeCount more than that)"
+            cupsCoffeeMadeCount < requiredCoffeeCount -> "No, I can make only $cupsCoffeeMadeCount cups of coffee"
+            else -> "Yes, I can make that amount of coffee"
+        }
+    }
+
+    /** @return number of coffee cups cooked */
+    private fun makeCupsOfCoffee(requiredCoffeeCount: Int): Int {
+        val possibleCoffeeCups = howMuchCoffeePossible(ingredients)
+        val cupToMake = min(requiredCoffeeCount, possibleCoffeeCups)
+
+        ingredients.waterMl = ingredients.waterMl - cupToMake * WaterMlPerCoffeeCup
+        ingredients.coffeeBeans = ingredients.coffeeBeans - cupToMake * BeansPerCoffeeCup
+        ingredients.milkMl = ingredients.milkMl - cupToMake * MilkMlPerCoffeeCup
+
+        return cupToMake
+    }
+
+    private fun howMuchCoffeePossible(ingredients: Ingredients): Int {
+        val coffeeFromWater = ingredients.waterMl / WaterMlPerCoffeeCup
+        val coffeeFromBeans = ingredients.coffeeBeans / BeansPerCoffeeCup
+        val coffeeFromMilk = ingredients.milkMl / MilkMlPerCoffeeCup
+
+        return minOf(coffeeFromWater, coffeeFromBeans, coffeeFromMilk)
+    }
+
+    @Suppress("unused")
+    private fun sumIngredients(additionalIngredients: Ingredients) {
+        ingredients.run {
+            waterMl += additionalIngredients.waterMl
+            milkMl += additionalIngredients.milkMl
+            coffeeBeans += additionalIngredients.coffeeBeans
+        }
+    }
+
+    private fun getAdditionalIngredients(): Ingredients {
+        val additionalIngredients = Ingredients()
+
+        getIngredientAmount(Ingredient.Water)?.run {
+            additionalIngredients.waterMl += this
+        }
+        getIngredientAmount(Ingredient.Milk)?.run {
+            additionalIngredients.milkMl += this
+        }
+        getIngredientAmount(Ingredient.CoffeeBeans)?.run {
+            additionalIngredients.coffeeBeans += this
+        }
+
+        return additionalIngredients
+    }
+
+    private fun getIngredientAmount(type: Ingredient): Int? {
+        println("Write how many ${type.unitPluralShort} of ${type.namePlural} the coffee machine has")
+        return readLine()!!.toIntOrNull()
+    }
+
+    @Suppress("unused")
     private fun printIngredients(coffeeCount: Int) {
         print(getIngredientsMessage(coffeeCount))
     }
@@ -27,7 +101,7 @@ class CoffeeMachine {
             """.trimIndent()
     }
 
-    private fun howMuchCoffee(): Int {
+    private fun howMuchCoffeeRequired(): Int {
         println("Write how many cups of coffee you will need:")
         val cups = readLine()?.toIntOrNull()
         if (cups != null && cups >= 0) {
@@ -35,4 +109,11 @@ class CoffeeMachine {
         }
         return 0 // todo Fehlerbehandlung
     }
+
+    companion object {
+        const val WaterMlPerCoffeeCup = 200
+        const val BeansPerCoffeeCup = 15
+        const val MilkMlPerCoffeeCup = 50
+    }
 }
+
---
+++ Coffee Machine/task/src/machine/Ingredients.kt
@@ -0,0 +1,14 @@
+package machine
+
+data class Ingredients(
+        var waterMl: Int = 0,
+        var milkMl: Int = 0,
+        var coffeeBeans: Int = 0)
+
+enum class Ingredient(
+        val namePlural: String,
+        val unitPluralShort: String) {
+    Water("water", "ml"),
+    Milk("milk", "ml"),
+    CoffeeBeans("coffee beans", "grams")
+}

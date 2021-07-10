1f7dc9de4ba5a09bb807a97d9b729b63b25bb734
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2021-07-10 20:53:08 +0200

    Start Coffemachine 4, 1 small lesson task

---
+++ Topics/String basics/JSON/src/Main.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val output = """
+        {
+            "firstName": "John",
+            "lastName": "Smith",
+            "age": 35,
+            "phoneNumbers": [
+                {
+                    "type": "mobile",
+                    "number": "123 567-7890"
+                }
+            ]
+        }
+    """.trimIndent()
+
+    println(output)
+}

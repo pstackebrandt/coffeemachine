276903b13587c77fc909b64def5904b9678ef9ab
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2021-07-18 09:42:55 +0200

    Add loop for action and exit

--- Coffee Machine/task/src/machine/Action.kt
+++ Coffee Machine/task/src/machine/Action.kt
@@ -6,31 +6,35 @@ enum class ActionType(val text: String, val description: String) {
     BUY("buy", "Buy something"),
     FILL("fill", "Refill ingredients and cups"),
     TAKE("take", "Take money from machine"),
+    REMAINING("remaining", "Take money from machine"),
+    EXIT("exit", "Back - to main menu"),
 }
 
 class Action {
     /** Ask for action, fulfill action*/
-    fun manageAction(ingredients: IngredientStore) {
+    fun manageAction(ingredients: IngredientStore) : String {
         val action: ActionType = askForAction()
-        doAction(action, ingredients)
-
         if (showComments) println("Action: $action")
+        return doAction(action, ingredients)
     }
 
     /** Do expected action or nothing. */
-    private fun doAction(action: ActionType, ingredients: IngredientStore) {
+    private fun doAction(action: ActionType, ingredients: IngredientStore) : String{
         when (action) {
             ActionType.BUY -> Buying(ingredients).buy()
             ActionType.FILL -> ingredients.fillStore()
             ActionType.TAKE -> ingredients.take()
+            ActionType.REMAINING -> ingredients.printIngredients()
+            ActionType.EXIT -> return ActionType.EXIT.name
             else -> {
                 println("doAction: ActionType ${action.name} not supported")
             }
         }
+        return "ok"
     }
 
     private fun askForAction(): ActionType {
-        println("Write action (buy, fill, take):")
+        println("Write action (buy, fill, take, remaining, exit):")
 
         do {
             val input = InputGetter.getCommandText()
@@ -40,6 +44,8 @@ class Action {
                 ActionType.BUY.text -> return ActionType.BUY
                 ActionType.FILL.text -> return ActionType.FILL
                 ActionType.TAKE.text -> return ActionType.TAKE
+                ActionType.REMAINING.text -> return ActionType.REMAINING
+                ActionType.EXIT.text -> return ActionType.EXIT
             }
         } while (true)
     }
--- Coffee Machine/task/src/machine/CoffeeMachine.kt
+++ Coffee Machine/task/src/machine/CoffeeMachine.kt
@@ -1,6 +1,5 @@
 package machine
 
-import machine.product.Ingredient
 import machine.product.IngredientStore
 
 const val showComments = false
@@ -9,18 +8,14 @@ class CoffeeMachine(private val ingredients: IngredientStore) {
 
     fun start() {
         if (showComments) println("start")
-        var loop: Boolean
+        var loop = true
 
         do {
             ingredients.printIngredients()
+            repeat(2) { println() }
+            val actionResult = Action().manageAction(ingredients)
             println()
-            println()
-            Action().manageAction(ingredients)
-            println()
-            //sumIngredients(getAdditionalIngredients())
-            //printPossibleCoffeeAnswer(howMuchCoffeeRequired())
-            ingredients.printIngredients()
-            loop = false // currently no looping required
+            if (actionResult == ActionType.EXIT.name) loop = false
         } while (loop)
         if (showComments) println("end of start")
     }
